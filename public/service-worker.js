07421e30c340eef6830a2b04a497fcaa6478d73e
JacobRyanHanson <jacobryanhanson@gmail.com>
2022-01-29 16:41:45 -0600

    Update try/catch

--- public/service-worker.js
+++ public/service-worker.js
@@ -52,7 +52,7 @@ self.addEventListener('fetch', function (e) {
                     console.log('file is not cached, fetching : ' + e.request.url)
                     return fetch(e.request)
                 } catch {
-                    console.error(error);
+                    console.log(error);
                 }
             }
 
